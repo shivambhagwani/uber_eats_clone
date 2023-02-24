19e6b981a47ef32a01ee14c56b4d1565eba69fbb
Shivam Bhagwani <shivambhagwani@shivams-mbp.mynetworksettings.com>
2023-02-24 13:33:03 -0500

    CustomerAddress entity added.

---
+++ src/main/java/com/ubereats/ubereatsclone/entities/CustomerAddress.java
@@ -0,0 +1,24 @@
+package com.ubereats.ubereatsclone.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "address")
+public class CustomerAddress {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long addressId;
+
+    private String streetAddress;
+    private String city;
+
+    private String state;
+
+    private String pincode;
+
+}
