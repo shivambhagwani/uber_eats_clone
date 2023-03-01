64ac29cc6704a329c646b1eae69a5df3e6453153
Shivam Bhagwani <shivambhagwani@Shivams-MBP.fios-router.home>
2023-03-01 16:59:16 -0500

    Restaurant Employee Dto Added

---
+++ src/main/java/com/ubereats/ubereatsclone/dtos/RestaurantEmployeeDto.java
@@ -0,0 +1,18 @@
+package com.ubereats.ubereatsclone.dtos;
+
+import com.ubereats.ubereatsclone.entities.Restaurant;
+import com.ubereats.ubereatsclone.entities.RestaurantEmployeeEnum;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class RestaurantEmployeeDto {
+
+    private String name;
+    private Integer age;
+    private Restaurant restaurant;
+    private RestaurantEmployeeEnum job_role;
+}
