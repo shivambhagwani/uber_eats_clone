3690e06619a4fb52894a4d755326b3a6aab9e5fd
shivambhagwani <125299494+shivambhagwani@users.noreply.github.com>
2023-03-07 17:01:34 -0500

    Day end commit 03/07/2023. Trying out spring security on a dummy user. TBC

---
+++ src/main/java/com/ubereats/ubereatsclone/entities/Role.java
@@ -0,0 +1,23 @@
+package com.ubereats.ubereatsclone.entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Role {
+    @Id
+    @Column(name = "role_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+    public Integer getId() {
+        return id;
+    }
+}
---
+++ src/main/java/com/ubereats/ubereatsclone/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.ubereats.ubereatsclone.repositories;
+
+import com.ubereats.ubereatsclone.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
