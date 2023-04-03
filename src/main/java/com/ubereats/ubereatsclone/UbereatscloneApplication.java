b6a0162706fc9c2a0147b5a87c7538ec93092f19
shivambhagwani <125299494+shivambhagwani@users.noreply.github.com>
2023-04-03 11:20:25 -0400

    Enabling cache in the main class

--- src/main/java/com/ubereats/ubereatsclone/UbereatscloneApplication.java
+++ src/main/java/com/ubereats/ubereatsclone/UbereatscloneApplication.java
@@ -3,11 +3,13 @@ package com.ubereats.ubereatsclone;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@EnableCaching
 public class UbereatscloneApplication {
 
 	public static void main(String[] args) {
